2a87f47396ff7264f52aa72cb3851961a5981fe2
David de Jesus Costa <73839667+david-jc-br@users.noreply.github.com>
2022-09-02 18:36:35 -0300

    update

--- temp/h.sql
+++ temp/h.sql
@@ -1,1 +1,21 @@
 USE aluraDb;
+
+-- Criação de um usuário que tem permissão de selecionar dados de todas as tabelas.
+CREATE USER 'devDBA'@'localhost' IDENTIFIED BY '123456';
+
+GRANT SELECT ON aluraDb.* TO 'devDBA'@'localhost';
+
+DROP USER 'devDBA'@'localhost';
+
+/*-- Criação de um usuário que tem permissão de selecionar dados dos Alunos utilizados para entrega e
+-- executar um store producere.*/
+
+CREATE USER 'david'@'localhost' IDENTIFIED BY '123456';
+
+GRANT SELECT ON aluraDb.Aluno TO 'david'@'localhost';
+
+REVOKE SELECT ON aluraDb.Aluno FROM 'david'@'localhost';
+
+GRANT EXECUTE ON PROCEDURE aluraDb.InfoEmpresas TO 'david'@'localhost';
+
+DROP USER 'david'@'localhost';
