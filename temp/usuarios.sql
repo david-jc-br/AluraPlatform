bef2e1eaf36d618b01cfb6701aca5dfb3797b194
David de Jesus Costa <73839667+david-jc-br@users.noreply.github.com>
2022-09-06 18:55:40 -0300

    Done !

--- temp/usuarios.sql
+++ temp/usuarios.sql
@@ -1,4 +1,4 @@
--- Item H Exemplos de criação de usuários concessão e revocação de permissão de acesso 
+-- Item H, Exemplos de criação de usuários concessão e revocação de permissão de acesso 
 
 -- Criação e exclusão de um usuário que tem permissão de selecionar dados de todas as tabela do database aluraDb.
 CREATE USER 'devDBA'@'localhost' IDENTIFIED BY '123456';
@@ -7,7 +7,8 @@ GRANT SELECT ON aluraDb.* TO 'devDBA'@'localhost';
 
 DROP USER 'devDBA'@'localhost';
 
-/*-- Criação e exclusão de um usuário que tem permissão de selecionar dados dos Alunos utilizados para entrega e executar um store producere.*/
+-- Criação e exclusão de um usuário que tem permissão de selecionar
+-- dados dos Alunos utilizados para entrega e executar um store producere.
 
 CREATE USER 'davidjc'@'localhost' IDENTIFIED BY '123456';
 
