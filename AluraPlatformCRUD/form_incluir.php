4260f001988345268cb6d2433bd260870d2c5551
David de Jesus Costa <73839667+david-jc-br@users.noreply.github.com>
2022-09-10 09:20:57 -0300

    CRUD Done!

---
+++ AluraPlatformCRUD/form_incluir.php
@@ -0,0 +1,49 @@
+<?php
+header("Content-Type: text/html; charset=UTF-8",true);
+?>
+<html>
+<head><title>Inserir/Editar uma Curso.</title></head>
+<body>
+<body style ="background-color:#63A5AF;">
+<form name="form1" method="POST" action="incluir.php">
+<?php
+if(isset($_GET["idCurso"])){
+  include("./config.php");
+  $con = mysqli_connect($host, $login, $senha, $bd);
+?>
+  <center><h3>Editar Curso</h3></center>
+<?php
+  $sql = "SELECT Curso.*
+          FROM Curso
+          WHERE idCurso=".$_GET['idCurso'];
+  $result = mysqli_query($con, $sql);
+  $vetor = mysqli_fetch_array($result, MYSQLI_ASSOC);
+  mysqli_close($con);
+?>
+  <input type="hidden" name="idCurso" value="<?php echo $_GET['idCurso']; ?>">
+<?php
+}else{
+?>
+  <center><h3>Cadastrar novo Curso</h3></center>
+<?php
+}
+?>
+<table border="0" align="center" width="60%">
+<tr><td width="20%">Nome Do Curso:</td>
+    <td colspan="2" width="90%">
+    <input type="text" name="nomeCurso" value="<?php echo @$vetor['nomeCurso']; ?>" maxlength="70" size="70">
+  </td>
+</tr>
+<tr><td width="20%">Tipo de Plano:</td>
+    <td colspan="1" width="90%">
+	  <input type="text" name="plano" value="<?php echo @$vetor['plano']; ?>" maxlength="70" size="70">
+</tr>
+<tr><td colspan="5" align="center">
+      <input type="submit" value="Salvar">
+      <input type="button" value="Cancelar" onclick="location.href='index.php'">
+    </td>
+</tr>
+</table>
+</form>
+</body>
+</html>
